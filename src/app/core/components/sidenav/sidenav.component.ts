1d075b12d84dbae81f3f33d6f80b2bfc85b77671
Zabi Babar <zabiubabar@gmail.com>
2023-01-16 12:52:37 -0500

    Remove toolbar

--- src/app/core/components/sidenav/sidenav.component.ts
+++ src/app/core/components/sidenav/sidenav.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core'
-import { OverlayContainer } from '@angular/cdk/overlay'
 import { NavList } from '../../types/nav-List'
 
 @Component({
@@ -8,23 +7,10 @@ import { NavList } from '../../types/nav-List'
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent {
-  themeColor: 'primary' | 'accent' | 'warn' = 'primary'
-  isDark = false
   navList: NavList[] = [
     { icon: 'home', label: 'Home' },
     { icon: 'event', label: 'Events' },
     { icon: 'groups', label: 'Groups' },
     { icon: 'chat', label: 'Messages' }
   ]
-
-  constructor(private overlayContainer: OverlayContainer) {}
-
-  toggleTheme(): void {
-    this.isDark = !this.isDark
-    if (this.isDark) {
-      this.overlayContainer.getContainerElement().classList.add('dark-theme')
-    } else {
-      this.overlayContainer.getContainerElement().classList.remove('dark-theme')
-    }
-  }
 }
