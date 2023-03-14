6a9bf0fdd50c3d9536ed5aaca989d0da62f6badc
Zabi Babar <zabiubabar@gmail.com>
2023-03-14 19:44:33 -0400

    Add container to page container and remove sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core'
+import { Store } from '@ngrx/store'
+import { selectCurrentUser } from './features/users/store/user/user.selectors'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'events-ui'
+
+  user$ = this.store.select(selectCurrentUser)
+
+  constructor(private store: Store) {}
 }
--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { Store } from '@ngrx/store'
 import { selectCurrentUser } from 'src/app/features/users/store/user/user.selectors'
+import { NavLink } from '../../types/nav-link'
 
 @Component({
   selector: 'app-toolbar',
@@ -8,6 +9,11 @@ import { selectCurrentUser } from 'src/app/features/users/store/user/user.select
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
+  navList: NavLink[] = [
+    { icon: 'home', label: 'Home', link: '/' },
+    { icon: 'groups', label: 'Groups', link: '/groups' }
+    // { icon: 'event', label: 'Events', link: '/events' }
+  ]
   user$ = this.store.select(selectCurrentUser)
 
   constructor(private readonly store: Store) {}
