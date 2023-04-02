a5b0da853164f774a922c3008a7111dba0de3fe2
Zabi Babar <zabiubabar@gmail.com>
2023-04-01 22:01:17 -0400

    Show groups on home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Store } from '@ngrx/store'
+import { FetchAllGroupsActions } from 'src/app/features/groups/store/group.actions'
 
 @Component({
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-
+  constructor(private store: Store) {
+    this.store.dispatch(FetchAllGroupsActions.fetchAllGroups())
+  }
 }
--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common'
 import { GroupsPageRoutingModule } from './home-page-routing.module'
 import { HomePageComponent } from './home-page.component'
 import { MatButtonModule } from '@angular/material/button'
+import { CoreModule } from 'src/app/core/core.module'
+import { GroupsModule } from 'src/app/features/groups/groups.module'
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, GroupsPageRoutingModule, MatButtonModule]
+  imports: [CommonModule, GroupsPageRoutingModule, MatButtonModule, GroupsModule, CoreModule]
 })
 export class HomePageModule {}
