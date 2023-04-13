3fceb8dcdd81e61bf1915c7d7c2362137b523dd4
Zabi Babar <zabiubabar@gmail.com>
2023-04-13 13:36:45 -0400

    Change toolbar styles

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core'
 import { Store } from '@ngrx/store'
 import { selectCurrentUser } from 'src/app/features/users/store/user/user.selectors'
-import { NavLink } from '../../types/nav-link'
 
 @Component({
   selector: 'app-toolbar',
@@ -9,11 +8,6 @@ import { NavLink } from '../../types/nav-link'
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
-  navList: NavLink[] = [
-    { icon: 'home', label: 'Home', link: '/' },
-    { icon: 'groups', label: 'Groups', link: '/groups' }
-    // { icon: 'event', label: 'Events', link: '/events' }
-  ]
   user$ = this.store.select(selectCurrentUser)
 
   constructor(private readonly store: Store) {}
