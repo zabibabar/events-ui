38c788ca7e322f94a6ebdde0d5ca0bfebb88857b
Zabi Babar <zabiubabar@gmail.com>
2023-04-13 15:50:22 -0400

    Responsive group events page

--- src/app/pages/group-events-page/group-events-page.component.ts
+++ src/app/pages/group-events-page/group-events-page.component.ts
@@ -1,13 +1,18 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
 import { Component } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { Store } from '@ngrx/store'
-import { Observable } from 'rxjs'
+import { Observable, map } from 'rxjs'
 import { Event } from 'src/app/features/events/interfaces/event'
 import {
   selectPastEventsByCurrentGroup,
   selectUpcomingEventsByCurrentGroup
 } from 'src/app/features/events/store/event.selectors'
-import { selectCurrentGroup } from 'src/app/features/groups/store/group.selectors'
+import {
+  selectCurrentGroup,
+  selectPastEventCountForCurrentGroup,
+  selectUpcomingEventCountForCurrentGroup
+} from 'src/app/features/groups/store/group.selectors'
 
 @Component({
   templateUrl: './group-events-page.component.html',
@@ -19,6 +24,11 @@ export class GroupEventsPageComponent {
   events$: Observable<Event[]> = this.store.select(
     this.isPastPage ? selectPastEventsByCurrentGroup({ limit: 10 }) : selectUpcomingEventsByCurrentGroup({ limit: 10 })
   )
+  upcomingEventCount$: Observable<number | undefined> = this.store.select(selectUpcomingEventCountForCurrentGroup)
+  pastEventCount$: Observable<number | undefined> = this.store.select(selectPastEventCountForCurrentGroup)
+  isMobile$: Observable<boolean>
 
-  constructor(private store: Store, private route: ActivatedRoute) {}
+  constructor(private store: Store, private route: ActivatedRoute, breakpoints: BreakpointObserver) {
+    this.isMobile$ = breakpoints.observe(Breakpoints.XSmall).pipe(map(({ matches }) => matches))
+  }
 }
