1c818f4b315ad57d2da070c82754906e7aa2256f
Zabi Babar <zabiubabar@gmail.com>
2023-05-02 21:28:36 -0400

    Added load more button and action to event list

--- src/app/features/events/components/event-list/event-list.component.ts
+++ src/app/features/events/components/event-list/event-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, Input } from '@angular/core'
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core'
 import { Event } from '../../interfaces/event'
+import { Store } from '@ngrx/store'
+import { selectIsLoadingEventAction } from '../../store/event.selectors'
 
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.scss']
+  styleUrls: ['./event-list.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class EventListComponent {
   @Input() events: Event[] = []
+  @Input() showMoreOption = true
+  @Output() next = new EventEmitter<void>()
+
+  loading$ = this.store.select(selectIsLoadingEventAction)
+
+  constructor(private store: Store) {}
 
   eventTrackBy(_: number, event: Event) {
     return event.id
+  }
+
+  fetchNextEvents(): void {
+    this.next.emit()
   }
 }
